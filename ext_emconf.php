32df29e585208d53e83d81fd1fa28435814d8326
uhlmann <uhlmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-02 12:39:27 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/shadowbox/trunk@9822 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "shadowbox"
+#
+# Auto generated 02-06-2008 14:36
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Shadowbox',
+	'description' => 'Shadowbox is a cross-browser, cross-platform, cleanly-coded and fully-documented media viewer application written entirely in JavaScript. Using Shadowbox, website authors can display a wide assortment of media in all major browsers without navigating away from the linking page.',
+	'category' => 'fe',
+	'author' => 'Jonathan Uhlmann',
+	'author_email' => 'uhlmann@augenreiz.ch',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"4671";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"6fb5";s:19:"doc/wizard_form.dat";s:4:"1517";s:20:"doc/wizard_form.html";s:4:"8020";s:30:"static/shadowbox/constants.txt";s:4:"a415";s:26:"static/shadowbox/setup.txt";s:4:"ad23";}',
+);
+
+?>
